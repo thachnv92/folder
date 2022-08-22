a3855fef5f91989fe3c965b2aa0bc74d78cf3f09
Thach Nguyen Van <thachnv@ssi.com.vn>
2022-08-22 22:02:49 +0700

    Update existing folder

---
+++ cloudformation/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = format("nginx-%02d", count.index + 1)
+  count = 1
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
